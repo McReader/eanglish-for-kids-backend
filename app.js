f8650e6f675112e9deeb9068dd4b09af36686638
Denis Roy <mcreader215@gmail.com>
2021-08-21 21:41:45 +0100

    removed static files

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const categoriesRouter = require('./routes/categories');
+const categoriesRouter = require("./routes/categories");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/categories', categoriesRouter);
+app.use("/api/categories", categoriesRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
