12875ff3c28de9b894fc6f3bfd6950a6b6557e1b
Denis Roy <mcreader215@gmail.com>
2021-09-14 16:59:38 +0100

    fixed code style

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const session = require('express-session');
-const cors = require('cors');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
+const cors = require("cors");
 
 const { SESSION_SECRET } = process.env;
 
-const passport = require('./passport');
+const passport = require("./feature/iam/service/passport");
 
-const authRouter = require('./routes/auth');
-const categoriesRouter = require('./routes/categories');
+const authRouter = require("./feature/iam/controller/auth");
+const categoriesRouter = require("./feature/categories/controller/categories");
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(session({
-  secret: SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
-}));
+app.use(logger("dev"));
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -29,8 +30,8 @@ app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', authRouter);
-app.use('/api/categories', categoriesRouter);
+app.use("/", authRouter);
+app.use("/api/categories", categoriesRouter);
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ message: err.message });
